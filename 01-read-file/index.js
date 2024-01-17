28f6862b42ca4919edb0be2ebd33a640f0a2ceac
Raman Marсulevich <dwarf.md@gmail.com>
2024-01-17 21:54:07 +0100

    feat: add 01-read-file function

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs')
+
+
+async function readFile(readable) {
+	for await (const chunk of readable) {
+	  console.log(chunk);
+	}
+  }
+  
+  const stream = fs.createReadStream('./01-read-file/text.txt', {encoding: 'utf8'});
+
+readFile(stream);
+  
